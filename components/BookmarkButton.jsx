d98fc0f164023d90a983635d9dc5930cc13b4bd5
mackdr91 <domonick.r.mack+github@gmail.com>
2024-08-25 19:58:54 -0400

    something

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -3,10 +3,14 @@ import { FaBookmark } from "react-icons/fa";
 import bookmark from "@/app/actions/bookmark";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userID = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleClick = async () => {
     if (!userID) {
@@ -22,10 +26,15 @@ const BookmarkButton = ({ property }) => {
   });
 
   }
-  return (
-    <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2
+  return isBookmarked ? (
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2
     px-4 rounded-full flex items-center justify-center " onClick={handleClick}>
-      <FaBookmark class="mr-2" /> Bookmark Property
+      <FaBookmark className="mr-2" /> Bookmark Property
+    </button>
+  ) : (
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2
+    px-4 rounded-full flex items-center justify-center " onClick={handleClick}>
+      <FaBookmark className="mr-2" /> Bookmark Property
     </button>
   );
 };
