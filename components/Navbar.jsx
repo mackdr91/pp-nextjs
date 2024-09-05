c256a10f36613c636c2c9516abac773792ef8973
mackdr91 <domonick.r.mack+github@gmail.com>
2024-09-05 06:22:49 -0400

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -257,11 +257,22 @@ const Navbar = () => {
               </Link>
             )}
             {!session && (
-              <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-                <i className='fa-brands fa-google mr-2'></i>
-                <span>Login or Register</span>
-              </button>
-            )}
+            <div className='hidden md:block md:ml-6'>
+              <div className='flex items-center'>
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      key={index}
+                      onClick={() => signIn(provider.id)}
+                      className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    >
+                      <FaGoogle className='text-white mr-2' />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
+            </div>
+          )}
           </div>
         </div>
       )}
